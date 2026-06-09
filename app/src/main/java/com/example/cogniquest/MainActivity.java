8168c33dcb33f26c8c044a9d0b0cf5fab741bd23
liebertsenku <farelfarelbb@gmail.com>
2026-06-09 21:10:09 +0800

    feat: implement user registration and login functionality

--- app/src/main/java/com/example/cogniquest/MainActivity.java
+++ app/src/main/java/com/example/cogniquest/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.cogniquest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        
+        UserManager userManager = new UserManager(this);
+        Intent intent;
+        if (userManager.isLoggedIn()) {
+            if (userManager.getUsername().equalsIgnoreCase("admin")) {
+                intent = new Intent(this, AdminDashboardActivity.class);
+            } else {
+                intent = new Intent(this, HomeDashboardActivity.class);
+            }
+        } else {
+            intent = new Intent(this, LoginActivity.class);
+        }
+
+        startActivity(intent);
+        finish();
     }
 }
